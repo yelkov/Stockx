96c4b392f4551ab89aca8e7a917623065d3d352b
Yelko Veiga <yelkovq@gmail.com>
2024-03-05 20:37:48 +0100

    feat(Sale): creating Sale class with no compareTo

---
+++ app/src/main/java/edu/badpals/stockx/item/Sale.java
@@ -0,0 +1,23 @@
+package edu.badpals.stockx.item;
+
+public class Sale implements Offer{
+    private String size;
+    private Integer sale;
+
+    public void sale(String size,Integer sale){
+        this.size = size;
+        this.sale = sale;
+    }
+    @Override
+    public String size(){
+        return this.size;
+    }
+    @Override
+    public int value(){
+        return this.sale;
+    }
+    @Override
+    public int compareTo(Offer offer){
+        return 5;
+    }
+}
