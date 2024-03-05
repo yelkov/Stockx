6d36bf1199b37ee8c56c88b9242171f9bf4e983a
Yelko Veiga <yelkovq@gmail.com>
2024-03-06 00:18:33 +0100

    test(SneakerTest): adding addOfferBid

--- app/src/test/java/edu/badpals/stockx/item/SneakerTest.java
+++ app/src/test/java/edu/badpals/stockx/item/SneakerTest.java
@@ -14,4 +14,11 @@ public class SneakerTest {
         assertEquals("555088-105", sneaker.getStyle());
         assertEquals("Jordan 1", sneaker.getName());
     }
+    @Test
+    public void addOfferBid() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Bid bid = new Bid("9.5", 282);
+        sneaker.add(bid);
+        assertEquals(1, sneaker.offers().size());
+    }
 }
