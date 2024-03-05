e1b47f0e8f17737a9c816b69a3e1ae8cc11c4ce9
Yelko Veiga <yelkovq@gmail.com>
2024-03-06 00:35:02 +0100

    test(SneakerTest): adding Ask and Offer test

--- app/src/test/java/edu/badpals/stockx/item/SneakerTest.java
+++ app/src/test/java/edu/badpals/stockx/item/SneakerTest.java
@@ -21,4 +21,23 @@ public class SneakerTest {
         sneaker.add(bid);
         assertEquals(1, sneaker.offers().size());
     }
+    @Test
+    public void addOfferAsk() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Ask ask = new Ask("15", 288);
+        sneaker.add(ask);
+        sneaker.add(new Ask("13", 333));
+        sneaker.add(new Ask("14", 340));
+        sneaker.add(new Ask("13", 341));
+        assertEquals(4, sneaker.offers().size());
+    }
+    @Test
+    public void addOffer() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        sneaker.add(new Bid("9.5", 282));
+        sneaker.add(new Ask("15", 288));
+        Offer ask = new Ask("13", 333);
+        sneaker.add(ask);
+        assertEquals(3, sneaker.offers().size());
+    }
 }
