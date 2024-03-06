f6d902ef7466413473af28b53643941a634c950b
Yelko Veiga <yelkovq@gmail.com>
2024-03-06 23:31:45 +0100

    test(BidsTest): adding bids n asks n no bids

--- app/src/test/java/edu/badpals/stockx/item/criteria/BidsTest.java
+++ app/src/test/java/edu/badpals/stockx/item/criteria/BidsTest.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import edu.badpals.stockx.criteria.Bids;
 import edu.badpals.stockx.criteria.Criteria;
+import edu.badpals.stockx.item.Ask;
 import edu.badpals.stockx.item.Bid;
 import edu.badpals.stockx.item.Offer;
 import edu.badpals.stockx.item.Sneaker;
@@ -28,5 +29,37 @@ public class BidsTest {
 
         List<Offer> filteredBids = bids.checkCriteria(sneaker);
         assertTrue(filteredBids.stream().allMatch(b -> b instanceof Bid));
+
+        Optional<Offer> maxOpt = Optional.ofNullable(bids.checkCriteria(sneaker).get(0));
+        sneaker.setBid(maxOpt.isPresent() ? maxOpt.get().value() : 0);
+        assertEquals(900, sneaker.getBid());
+    }
+    @Test
+    public void checkCriteria_bids_aks_Test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        sneaker.add(new Bid("5.5", 550));
+        sneaker.add(new Bid("4.5", 480));
+        sneaker.add(new Bid("5.5", 900));
+        sneaker.add(new Bid("6", 472));
+        sneaker.add(new Ask("15", 288));
+        sneaker.add(new Ask("13", 333));
+        sneaker.add(new Ask("14", 1000));
+        sneaker.add(new Ask("13", 341));
+
+        Criteria bids = new Bids();
+        List<Offer> filteredBids = bids.checkCriteria(sneaker);
+        assertTrue(filteredBids.stream().allMatch(b -> b instanceof Bid));
+
+        Optional<Offer> maxOpt = Optional.ofNullable(bids.checkCriteria(sneaker).get(0));
+        sneaker.setBid(maxOpt.isPresent()? maxOpt.get().value() : 0);
+        assertEquals(900, sneaker.getBid());
+    }
+    @Test
+    public void checkCriteria_no_bids_Test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Criteria maxBid = new Bids();
+        List<Offer> maximum = maxBid.checkCriteria(sneaker);
+        sneaker.setBid(maximum.isEmpty()? 0 : maximum.get(0).value());
+        assertEquals(0, sneaker.getBid());
     }
 }
